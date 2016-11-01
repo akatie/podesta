4726e8ae6eb4ee0c7d2f680614f5c8a9721e4baf
Matthias Johnson <matthias.a.johnson@gmail.com>
2016-11-01 09:44:54 -0400

    first draft with results

---
+++ podesta.py
@@ -0,0 +1,27 @@
+import dkim
+import urllib2
+import json
+import datetime
+
+url = "https://wikileaks.org/podesta-emails/get/"
+results = {}
+n = 1
+start = str(datetime.datetime.now())
+
+while True:
+    try:
+        fetch = urllib2.urlopen(url +str(n)).read()
+        email = dkim.DKIM(fetch)
+    except urllib2.URLError as url_error:
+        break
+    try:
+        results[n] = email.verify()
+        print("email", n, email.verify())
+    except dkim.DKIMException as dkim_error:
+        results[n] = str(dkim_error)
+        print("email", n, dkim_error)
+    n += 1
+with open("perm_results", "w") as f:
+    f.write("This script started at %s \n" % start)
+    f.write("completed at %s \n" % str(datetime.datetime.now()))
+    json.dump(results, f)
